be10b7c3cf29481ed803911beae725d8effb6129
jesse406 <ismailajesse@gmail.com>
2023-04-06 12:08:07 +0100

    key update

--- client/script.js
+++ client/script.js
@@ -33,6 +33,7 @@ function typeText(element, text) {
     }, 20)
 }
 
+
 // generate unique ID for each message div of bot
 // necessary for typing text effect for that specific reply
 // without unique ID, typing text will work on every element
@@ -86,7 +87,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://cpe.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
